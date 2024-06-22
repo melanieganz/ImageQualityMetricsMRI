9d3cd60666b68da47d9dfa1604be514243951c12
Elisa Marchetto <elisa.marchetto@nyulangone.org>
2024-06-21 21:43:39 -0400

    Fixed bugs creating mask for tse and tirm, fixed registration to reference per non-mprage sequences. Polished the code to make it more readable.

--- preprocessingData.py
+++ preprocessingData.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import subprocess
 import nibabel as nib
+import datalad
 
 # Function to execute system command and check output
 def execute_command(command):
@@ -11,20 +12,38 @@ def execute_command(command):
     return result.stdout.decode('utf-8')
 
 # Function to find all reference images in a directory for given sequences
-def find_reference_images(directory, sequences):
-    reference_images = {}
-    for seq in sequences:
-        reference_images[seq] = []
-        for filename in os.listdir(directory):
-            if seq.lower() in filename.lower() and filename.endswith(".nii"):
-                reference_images[seq].append(os.path.join(directory, filename))
+def find_reference_images(directory, seq):
+    reference_images = None 
+    
+    for filename in os.listdir(directory):
+        if seq.lower() in filename.lower() and filename.endswith(".nii") and "run-01" in filename and "pmcoff" in filename:
+            reference_images=os.path.join(directory, filename)
     return reference_images
+
+# ------------------------------------------------------------------------------------------------------
+# Pre-processing applied on data from:
+# Melanie Ganz and Hannah Eichhorn (2022). Datasets with and without deliberate head movements for 
+# evaluating the performance of markerless prospective motion correction and selective reacquisition 
+# in a general clinical protocol for brain MRI. 
+# ------------------------------------------------------------------------------------------------------
+# The data is available for download at OpenNeuro. 
+# ------------------------------------------------------------------------------------------------------
+# Before downloading, make sure you have Datalad installed:
+    # pip install datalad[full] (pip)
+    # or
+    # conda install -c conda-forge datalad (conda)
+# And Git Annex: 
+    # sudo apt-get install git-annex (Debian/Ubuntu)
+    # or
+    # brew install git-annex (Mac)
+# ------------------------------------------------------------------------------------------------------
+
 
 # URL of the Git repository to clone
 repo_url = "https://github.com/OpenNeuroDatasets/ds004332.git"
 
 # Directory where you want to clone the repository
-data_dir = "test"
+data_dir = "OpenNeuro_dataset"
 
 # Check if the directory already exists
 if os.path.exists(data_dir):
@@ -36,6 +55,10 @@ else:
     # Execute the command
     try:
         subprocess.run(clone_command, check=True)
+        # Get the actual data        
+        get_data = ["datalad","get","-r",data_dir] 
+        subprocess.run(get_data, check=True)
+        
         print("Repository cloned successfully")
     except subprocess.CalledProcessError as e:
         print(f"Error cloning repository: {e}")
@@ -62,15 +85,14 @@ for subject_folder in subject_folders:
             
             if seq == "mprage":
                 # For MPRAGE sequence, run BET if it is the reference
-                bet_output_dir = os.path.join(sequence_output_dir, "bet_output")
-                os.makedirs(bet_output_dir, exist_ok=True)
+                mprage_bet = os.path.join(sequence_output_dir, "bet_mprage")
                 
-                bet_command = ['bet', ref_image, bet_output_dir, '-R', '-f', '0.5', '-g', '0', '-m']
+                bet_command = ['bet', ref_image, mprage_bet, '-R', '-f', '0.4', '-t', '-m']
                 subprocess.run(bet_command, check=True)
                 print(f"BET completed for {subject_folder} ({seq})")
                 
                 # Get the MPRAGE BET mask file
-                mprage_bet_mask = os.path.join(bet_output_dir, "mprage_mask.nii.gz")
+                mprage_bet_mask = os.path.join(sequence_output_dir, "bet_mprage_mask.nii.gz")
                 
                 # Register non-reference MPRAGE images to their corresponding MPRAGE references
                 for filename in os.listdir(anat_folder):
@@ -79,32 +101,34 @@ for subject_folder in subject_folders:
                         flirt_output_dir = os.path.join(data_dir, subject_folder, seq.lower())
                         os.makedirs(flirt_output_dir, exist_ok=True)
                         
-                        flirt_output = os.path.join(flirt_output_dir, f"align_img.nii.gz")
+                        flirt_output = os.path.join(flirt_output_dir, f"align_{filename}")
                         
-                        flirt_command = ["flirt", "-in", input_image, "-ref", ref_image, "-out", flirt_output, "-applyxfm"]
+                        flirt_command = ["flirt", "-in", input_image, "-ref", ref_image, "-out", flirt_output,'-dof','6']
                         subprocess.run(flirt_command, check=True)
                         print(f"FLIRT completed for {filename} in {subject_folder} ({seq})")                               
                                         
             else:
                 # For other sequences, register the MPRAGE BET mask to the reference image using FLIRT
-                flirt_output_dir = os.path.join(sequence_output_dir, "flirt_output")
+                flirt_output_dir = os.path.join(data_dir, subject_folder, seq.lower())
                 os.makedirs(flirt_output_dir, exist_ok=True)
                 
                 flirt_output = os.path.join(flirt_output_dir, f"align_{seq}_mask.nii.gz")
+                flirt_command = ["flirt", "-in", mprage_bet_mask, "-ref", ref_image, "-out", flirt_output,'-dof','6']
                 
-                flirt_command = ["flirt", "-in", mprage_bet_mask, "-ref", ref_image, "-out", flirt_output, "-applyxfm"]
                 subprocess.run(flirt_command, check=True)
                 print(f"FLIRT mask completed for {subject_folder} ({seq})")
     
                 # Register non-reference images of other sequences to their corresponding references        
                 for filename in os.listdir(anat_folder):
-                    if filename.endswith(".nii") and filename != os.path.basename(ref_image):
+                    if filename.endswith(".nii") and filename != os.path.basename(ref_image) and seq in filename:
                         input_image = os.path.join(anat_folder, filename)
                         flirt_output_dir = os.path.join(data_dir, subject_folder, seq.lower())
                         os.makedirs(flirt_output_dir, exist_ok=True)
                         
-                        flirt_output = os.path.join(flirt_output_dir, f"align_img.nii.gz")
+                        flirt_output = os.path.join(flirt_output_dir, f"align_{filename}.nii.gz")
                         
-                        flirt_command = ["flirt", "-in", input_image, "-ref", ref_image, "-out", flirt_output, "-applyxfm"]
+                        flirt_command = ["flirt", "-in", input_image, "-ref", ref_image, "-out", flirt_output]
                         subprocess.run(flirt_command, check=True)
                         print(f"FLIRT completed for {filename} in {subject_folder} ({seq})")
+                        
+    print(f"Process completed for {subject_folder}")
