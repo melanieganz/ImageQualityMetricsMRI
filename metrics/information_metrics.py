162335ea7d201eebda0470e59850c374c825b357
HannahEichhorn <hannah.eichhorn@web.de>
2024-07-04 12:03:36 +0200

    Update VIF (reduction method)

--- metrics/information_metrics.py
+++ metrics/information_metrics.py
@@ -2,7 +2,7 @@ import piq
 import torch
 
 
-def vif(img, img_ref):
+def vif(img, img_ref, reduction='mean'):
     """ Calculate the visual information fidelity metric between two images.
 
     The code is based on the article:
@@ -20,6 +20,9 @@ def vif(img, img_ref):
         image for which the metrics should be calculated.
     img_ref : numpy array
         reference image.
+    reduction : str
+        Reduction method for the VIF values of multiple slices. Options:
+        'mean' (default), 'worst'.
 
     Returns
     -------
@@ -31,4 +34,12 @@ def vif(img, img_ref):
     img = torch.from_numpy(img[:, None]).float()
     img_ref = torch.from_numpy(img_ref[:, None]).float()
 
-    return piq.vif_p(img, img_ref, data_range=1., sigma_n_sq=0.4)
+    vif_values = piq.vif_p(img, img_ref, data_range=1., sigma_n_sq=0.4,
+                           reduction='none')
+
+    if reduction == 'mean':
+        return torch.mean(vif_values).item()
+    elif reduction == 'worst':
+        return torch.min(vif_values).item()
+    else:
+        raise ValueError(f"Reduction method {reduction} not supported.")
