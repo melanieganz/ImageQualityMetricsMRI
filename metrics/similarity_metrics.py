5a1e18f643ae0058b4a62ca2be3341326e2a347c
HannahEichhorn <hannah.eichhorn@web.de>
2024-07-04 11:30:20 +0200

    Update FSIM (implementation and reduction method)

--- metrics/similarity_metrics.py
+++ metrics/similarity_metrics.py
@@ -1,20 +1,25 @@
-import numpy as np
-import image_similarity_measures as img_sim
+import piq
+import torch
 
 
-def fsim(img, img_ref):
+def fsim(img, img_ref, reduction='mean'):
     """Calculate FSIM between two 3D images slice-wise.
 
     Notes:
         - Slice dimension is assumed to be the first dimension.
-        - The images are assumed to be masked and normalised to [0, 255].
+        - The images are assumed to be masked (by multiplication)
+          and normalised to [0, 1].
+        - The final metric value is calculated as mean or min over all slices.
     """
-        
-    fsims = []
-    for i in range(len(img)):
-        fsims.append(img_sim.quality_metrics.fsim(
-            org_img=img_ref[i][:,  :, None],
-            pred_img=img[i][:,  :, None])
-        )
 
-    return np.mean(fsims)
+    img = torch.from_numpy(img[:, None]).float()
+    img_ref = torch.from_numpy(img_ref[:, None]).float()
+    fsim_values = piq.fsim(img, img_ref, data_range=1.0, reduction='none',
+                           chromatic=False)
+
+    if reduction == 'mean':
+        return torch.mean(fsim_values).item()
+    elif reduction == 'min':
+        return torch.min(fsim_values).item()
+    else:
+        raise ValueError(f"Reduction method {reduction} not supported.")
