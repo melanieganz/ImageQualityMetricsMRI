057616799bc9c6294f13acf8aa1ed371b03f1f99
Hannah Eichhorn <hannah.eichhorn@helmholtz-munich.de>
2024-08-21 15:21:58 +0200

    Sort out reference images for full-reference metrics

--- correlation_analysis.py
+++ correlation_analysis.py
@@ -6,23 +6,24 @@ import seaborn as sns
 
 
 def load_metrics_scores(input_csv):
-    metrics_to_include = ["SSIM", "PSNR", "FSIM", "VIF", "LPIPS", "AES", "TG",
-                          "NGS", "GE", "IE"]
+    full_reference_metrics = ["SSIM", "PSNR", "FSIM", "VIF", "LPIPS"]
+    reference_free_metrics = ["AES", "TG", "NGS", "GE", "IE"]
     observer_scores_to_include = ["Radiographer1", "Radiographer2",
                                   "Neuroradiologist"]
     sequences = ["mprage", "t1tirm", "flair", "t2tse"]
-    metrics = {
-        s : {m: [] for m in metrics_to_include} for s in sequences
-    }
-    observer_scores = {
-        s: {o: [] for o in observer_scores_to_include} for s in sequences
-    }
-    subjects = {
-        s: [] for s in sequences
-    }
-    acquisition = {
-        s: [] for s in sequences
-    }
+
+    metrics = {"full_ref": {s : {m: [] for m in full_reference_metrics}
+                             for s in sequences},
+               "ref_free": {s : {m: [] for m in reference_free_metrics}
+                            for s in sequences}}
+    observer_scores = {"full_ref": {s: {o: [] for o in observer_scores_to_include}
+                                    for s in sequences},
+                          "ref_free": {s: {o: [] for o in observer_scores_to_include}
+                                        for s in sequences}}
+    subjects = {"full_ref": {s: [] for s in sequences},
+                "ref_free": {s: [] for s in sequences}}
+    acquisition = {"full_ref": {s: [] for s in sequences},
+                   "ref_free": {s: [] for s in sequences}}
 
     data = np.loadtxt(input_csv, delimiter=',',
                       unpack=True, dtype=str)
@@ -44,21 +45,38 @@ def load_metrics_scores(input_csv):
         if sequence is None:
             raise ValueError(f"Sequence not recognized for file {file}")
 
-        subject = data[np.where(header == "Sbj")[0][0], i+1]
-        subjects[sequence].append(subject)
-        acquisition[sequence].append(file)
+        is_reference = "ref_" in file
 
-        for metric in metrics_to_include:
+        subject = data[np.where(header == "Sbj")[0][0], i+1]
+        subjects["ref_free"][sequence].append(subject)
+        acquisition["ref_free"][sequence].append(file)
+        if not is_reference:
+            subjects["full_ref"][sequence].append(subject)
+            acquisition["full_ref"][sequence].append(file)
+
+        for metric in full_reference_metrics:
             if metric not in header:
                 raise ValueError(f"No {metric} column found in the CSV file.")
-            metrics[sequence][metric].append(
+            if is_reference:
+                continue
+            metrics["full_ref"][sequence][metric].append(
+                data[np.where(header == metric)[0][0], i+1].astype(float)
+            )
+        for metric in reference_free_metrics:
+            if metric not in header:
+                raise ValueError(f"No {metric} column found in the CSV file.")
+            metrics["ref_free"][sequence][metric].append(
                 data[np.where(header == metric)[0][0], i+1].astype(float)
             )
 
         for observer in observer_scores_to_include:
             if observer not in header:
                 raise ValueError(f"No {observer} column found in the CSV file.")
-            observer_scores[sequence][observer].append(
+            if not is_reference:
+                observer_scores["full_ref"][sequence][observer].append(
+                    data[np.where(header == observer)[0][0], i+1].astype(float)
+                )
+            observer_scores["ref_free"][sequence][observer].append(
                 data[np.where(header == observer)[0][0], i+1].astype(float)
             )
 
@@ -75,15 +93,18 @@ def plot_correlation_heatmap(spearman_corr, original_metrics_order):
 
     # Create a matrix for the heatmap
     sequences = list(filtered_corr.keys())
-    metrics = original_metrics_order
+    metrics = [metric for key in original_metrics_order
+               for metric in original_metrics_order[key]]
     heatmap_data = np.zeros((len(sequences), len(metrics)))
 
     for i, sequence in enumerate(sequences):
         for j, metric in enumerate(metrics):
             heatmap_data[i, j] = filtered_corr[sequence].get(metric, np.nan)
 
-    # Plotting
-    fig, ax = plt.subplots(figsize=(12, 8))
+    fig_width = len(metrics) * 1.5 + 1.5
+    fig_height = len(sequences) * 1.5
+
+    fig, ax = plt.subplots(figsize=(fig_width, fig_height))
     sns.heatmap(heatmap_data, annot=True, fmt=".2f", cmap="coolwarm",
                 xticklabels=metrics, yticklabels=sequences, ax=ax, square=True,
                 cbar_kws={"shrink": 0.5})
@@ -97,28 +118,31 @@ def plot_correlation_heatmap(spearman_corr, original_metrics_order):
 
 
 def plot_scatter_plots(metrics, observer_scores, original_metrics_order):
-    for sequence in metrics.keys():
-        num_metrics = len(original_metrics_order)
+    types = list(metrics.keys())
+    for sequence in metrics[types[0]].keys():
         fig, axes = plt.subplots(nrows=3, ncols=4, figsize=(20, 15))
 
-        for i, metric in enumerate(original_metrics_order):
-            row = i // 4
-            col = i % 4
-            ax = axes[row, col]
-            x = metrics[sequence][metric]
-            y = observer_scores[sequence]["Combined"]
-            ax.scatter(x, y, alpha=0.7)
-            ax.set_xlabel(f'{metric}', fontsize=16)
-            ax.set_ylabel('Combined Observer Scores', fontsize=16)
-            ax.grid(True)
+        i = 0
+        for type in types:
+            for metric in original_metrics_order[type]:
+                row = i // 4
+                col = i % 4
+                ax = axes[row, col]
+                x = metrics[type][sequence][metric]
+                y = observer_scores[type][sequence]["Combined"]
+                ax.scatter(x, y, alpha=0.7)
+                ax.set_xlabel(f'{metric}', fontsize=16)
+                ax.set_ylabel('Combined Observer Scores', fontsize=16)
+                ax.grid(True)
 
-            # Calculate and plot regression line
-            if len(x) > 1 and len(y) > 1:
-                slope, intercept = np.polyfit(x, y, 1)
-                ax.plot(x, slope * np.array(x) + intercept, color='tab:green')
+                # Calculate and plot regression line
+                if len(x) > 1 and len(y) > 1:
+                    slope, intercept = np.polyfit(x, y, 1)
+                    ax.plot(x, slope * np.array(x) + intercept, color='tab:green')
+                i += 1
 
         # Hide any unused subplots
-        for j in range(i + 1, 12):
+        for j in range(i, 12):
             row = j // 4
             col = j % 4
             fig.delaxes(axes[row, col])
@@ -143,39 +167,44 @@ def main():
     subjects, acquisition, metrics, observer_scores = load_metrics_scores(args.input_csv)
 
     # Combine observer scores with double weighting for neuroradiologist
-    for sequence in observer_scores.keys():
-        observer_scores[sequence]["Combined"] = np.mean(
-            [observer_scores[sequence]["Radiographer1"],
-             observer_scores[sequence]["Radiographer2"],
-             observer_scores[sequence]["Neuroradiologist"],
-             observer_scores[sequence]["Neuroradiologist"]],
-            axis=0
-        )
+    for type in observer_scores.keys():
+        for sequence in observer_scores[type].keys():
+            observer_scores[type][sequence]["Combined"] = np.mean(
+                [observer_scores[type][sequence]["Radiographer1"],
+                 observer_scores[type][sequence]["Radiographer2"],
+                 observer_scores[type][sequence]["Neuroradiologist"],
+                 observer_scores[type][sequence]["Neuroradiologist"]],
+                axis=0
+            )
 
     # Calculate the spearman correlation coefficients between the metrics and observer scores:
     spearman_corr = {}
-    for sequence in observer_scores.keys():
-        spearman_corr[sequence] = {}
-        for metric in metrics[sequence].keys():
-            spearman_corr[sequence][metric] = {}
-            sp_corr, p_val = spearmanr(metrics[sequence][metric],
-                                       observer_scores[sequence]["Combined"])
-            spearman_corr[sequence][metric]["corr"] = sp_corr
-            spearman_corr[sequence][metric]["p_val"] = p_val
+    for type in observer_scores.keys():
+        for sequence in observer_scores[type].keys():
+            spearman_corr.setdefault(sequence, {})
+            for metric in metrics[type][sequence].keys():
+                spearman_corr[sequence][metric] = {}
+                sp_corr, p_val = spearmanr(metrics[type][sequence][metric],
+                                           observer_scores[type][sequence]["Combined"])
+                spearman_corr[sequence][metric]["corr"] = sp_corr
+                spearman_corr[sequence][metric]["p_val"] = p_val
 
-    original_metrics_order = ["SSIM", "PSNR", "FSIM", "VIF", "LPIPS", "AES", "TG", "NGS", "GE", "IE"]
+    original_metrics_order = {
+        "full_ref": ["SSIM", "PSNR", "FSIM", "VIF", "LPIPS"],
+        "ref_free": ["AES", "TG", "NGS", "GE", "IE"]
+    }
     plot_correlation_heatmap(spearman_corr, original_metrics_order)
 
     # print mean correlation coefficient for each metric over all sequences
-    for metric in original_metrics_order:
-        if all([spearman_corr[seq][metric]["p_val"] < 0.05
-                for seq in observer_scores.keys()]):
-            mean_corr = np.mean([spearman_corr[seq][metric]["corr"]
-                                 for seq in observer_scores.keys()])
-            print(f"Mean correlation coefficient for {metric}: {mean_corr}")
+    for type in original_metrics_order.keys():
+        for metric in original_metrics_order[type]:
+            if all([spearman_corr[seq][metric]["p_val"] < 0.05
+                    for seq in observer_scores["full_ref"].keys()]):
+                mean_corr = np.mean([spearman_corr[seq][metric]["corr"]
+                                     for seq in observer_scores["full_ref"].keys()])
+                print(f"Mean correlation coefficient for {metric}: {mean_corr}")
 
     plot_scatter_plots(metrics, observer_scores, original_metrics_order)
-
 
 
 if __name__ == "__main__":
